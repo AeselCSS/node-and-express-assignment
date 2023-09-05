6d1bff6b07b56041f78be9273b200d873a4a7dea
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-05 10:42:58 +0200

    initiate application

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,5 +1,8 @@
 
+import {getAll} from "./assets/modules/api.js";
+import {showArtists} from "./assets/modules/show-artists.js";
 
-document.addEventListener('DOMContentLoaded', (): void => {
-    console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', async (): Promise<void> => {
+    const artists = await getAll('artists');
+    await showArtists(artists)
 });
