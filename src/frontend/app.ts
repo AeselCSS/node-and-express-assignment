0316244260ea17752afdd33834b52524d5126cf2
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-06 00:05:50 +0200

    added event listeners and added refreshArtists to be fired upon load

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,8 +1,25 @@
 
 import {getAll} from "./assets/modules/api.js";
-import {showArtists} from "./assets/modules/show-artists.js";
+import {createArtistModal} from "./assets/modules/create-artist.js";
+import {refreshArtists} from "./assets/modules/update-lists.js";
 
 document.addEventListener('DOMContentLoaded', async (): Promise<void> => {
-    const artists = await getAll('artists');
-    await showArtists(artists)
+    console.log('DOM loaded - fetching artists');
+    const artistsArray = await getAll('artists');
+    await refreshArtists(artistsArray);
+
+
+    // event listeners
+    document.querySelector('#add')?.addEventListener('click', () => {
+        console.log('Add artist clicked');
+        createArtistModal();
+    });
+    document.querySelector('.close')?.addEventListener('click', () => {
+        const modal:HTMLDialogElement | null = document.querySelector('.modal');
+        if (modal !== null) {
+            modal.close();
+        } else {
+            console.error('No modal found in DOM');
+        }
+    });
 });
