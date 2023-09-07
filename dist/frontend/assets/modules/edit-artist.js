7bdbc00cd53d227abdeb4c7efe46f48b0d33c465
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-07 23:13:55 +0200

    code refactoring

--- dist/frontend/assets/modules/edit-artist.js
+++ dist/frontend/assets/modules/edit-artist.js
@@ -1,37 +1,17 @@
 import { getOne, update } from "./api.js";
 import { refreshArtists } from "./update-lists.js";
 async function editArtist(id) {
-    const artist = await getOne('artists', id.toString());
+    const artist = await fetchArtist(id);
+    if (!artist) {
+        return;
+    }
     const modal = document.querySelector('.modal');
     const modalContent = document.querySelector('#dialog-modal-content');
     if (!modalContent) {
         console.error('No modal content found in DOM');
         return;
     }
-    modalContent.innerHTML = '';
-    const html = `
-    <h2>Edit Artist</h2>
-    <form id="edit-artist-form">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" value="${artist.name}" required>
-        <label for="birthday">Birthday</label>
-        <input type="date" id="birthday" name="birthday" value="${artist.birthday}" required>
-        <label for="activeSince">Active Since</label>
-        <input type="number" id="activeSince" name="activeSince" value="${artist.activeSince}" required>
-        <label for="genres">Genres</label>
-        <input type="text" id="genres" name="genres" value="${artist.genres}" required>
-        <label for="labels">Labels</label>
-        <input type="text" id="labels" name="labels" value="${artist.labels}" required>
-        <label for="website">Website</label>
-        <input type="text" id="website" name="website" value="${artist.website}" required>
-        <label for="shortDescription">Short Description</label>
-        <input type="text" id="shortDescription" name="shortDescription" value="${artist.shortDescription}" required>
-        <label for="image">Image</label>
-        <input type="text" id="image" name="image" value="${artist.image}" required>
-        <button type="submit">Update</button>
-    </form>
-    `;
-    modalContent.insertAdjacentHTML('beforeend', html);
+    modalContent.innerHTML = createEditFormHTML(artist);
     const form = document.querySelector('#edit-artist-form');
     if (!form || !modal) {
         console.error('No form or modal found in DOM');
@@ -40,9 +20,41 @@ async function editArtist(id) {
     form.addEventListener('submit', (e) => updateArtist(e, modal, id));
     modal.showModal();
 }
+async function fetchArtist(id) {
+    try {
+        return await getOne('artists', id.toString());
+    }
+    catch (error) {
+        console.error('Error fetching artist:', error);
+        return null;
+    }
+}
+function createEditFormHTML(artist) {
+    return `
+    <h2>Edit Artist</h2>
+    <form id="edit-artist-form">
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" value="${artist.name}" required>
+      <label for="birthday">Birthday</label>
+      <input type="date" id="birthday" name="birthday" value="${artist.birthday}" required>
+      <label for="activeSince">Active Since</label>
+      <input type="number" id="activeSince" name="activeSince" value="${artist.activeSince}" required>
+      <label for="genres">Genres</label>
+      <input type="text" id="genres" name="genres" value="${artist.genres}" required>
+      <label for="labels">Labels</label>
+      <input type="text" id="labels" name="labels" value="${artist.labels}" required>
+      <label for="website">Website</label>
+      <input type="text" id="website" name="website" value="${artist.website}" required>
+      <label for="shortDescription">Short Description</label>
+      <input type="text" id="shortDescription" name="shortDescription" value="${artist.shortDescription}" required>
+      <label for="image">Image</label>
+      <input type="text" id="image" name="image" value="${artist.image}" required>
+      <button type="submit">Update</button>
+    </form>
+  `;
+}
 async function updateArtist(e, modal, id) {
     e.preventDefault();
-    const form = e.target;
     const updatedArtist = {
         name: getValue('#name'),
         birthday: getValue('#birthday'),
@@ -60,9 +72,9 @@ async function updateArtist(e, modal, id) {
     }
     modal.close();
     await refreshArtists(response);
-    function getValue(selector) {
-        const element = form.querySelector(selector);
-        return element.value;
-    }
+}
+function getValue(selector) {
+    const element = document.querySelector(selector);
+    return element.value;
 }
 export { editArtist };
--- src/frontend/assets/modules/edit-artist.ts
+++ src/frontend/assets/modules/edit-artist.ts
@@ -1,50 +1,71 @@
-import {getOne, update} from "./api.js";
-import {refreshArtists} from "./update-lists.js";
+import { getOne, update } from "./api.js";
+import { refreshArtists } from "./update-lists.js";
 
-async function editArtist(id:number) {
-    const artist = await getOne('artists', id.toString());
-    const modal: HTMLDialogElement | null = document.querySelector('.modal');
-    const modalContent: HTMLElement | null = document.querySelector('#dialog-modal-content');
+async function editArtist(id: number): Promise<void> {
+    const artist: Artist | null = await fetchArtist(id);
+
+    if (!artist) {
+        return;
+    }
+
+    const modal = document.querySelector('.modal') as HTMLDialogElement | null;
+    const modalContent = document.querySelector('#dialog-modal-content');
+
     if (!modalContent) {
         console.error('No modal content found in DOM');
         return;
     }
-    modalContent.innerHTML = '';
-    const html = /*html*/ `
-    <h2>Edit Artist</h2>
-    <form id="edit-artist-form">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" value="${artist.name}" required>
-        <label for="birthday">Birthday</label>
-        <input type="date" id="birthday" name="birthday" value="${artist.birthday}" required>
-        <label for="activeSince">Active Since</label>
-        <input type="number" id="activeSince" name="activeSince" value="${artist.activeSince}" required>
-        <label for="genres">Genres</label>
-        <input type="text" id="genres" name="genres" value="${artist.genres}" required>
-        <label for="labels">Labels</label>
-        <input type="text" id="labels" name="labels" value="${artist.labels}" required>
-        <label for="website">Website</label>
-        <input type="text" id="website" name="website" value="${artist.website}" required>
-        <label for="shortDescription">Short Description</label>
-        <input type="text" id="shortDescription" name="shortDescription" value="${artist.shortDescription}" required>
-        <label for="image">Image</label>
-        <input type="text" id="image" name="image" value="${artist.image}" required>
-        <button type="submit">Update</button>
-    </form>
-    `;
-    modalContent.insertAdjacentHTML('beforeend', html);
-    const form = document.querySelector('#edit-artist-form');
+
+    modalContent.innerHTML = createEditFormHTML(artist);
+
+    const form = document.querySelector('#edit-artist-form') as HTMLFormElement | null;
+
     if (!form || !modal) {
         console.error('No form or modal found in DOM');
         return;
     }
+
     form.addEventListener('submit', (e) => updateArtist(e, modal, id));
     modal.showModal();
 }
 
-async function updateArtist(e: Event, modal: HTMLDialogElement, id: number) {
+async function fetchArtist(id: number): Promise<Artist | null> {
+    try {
+        return await getOne('artists', id.toString());
+    } catch (error) {
+        console.error('Error fetching artist:', error);
+        return null;
+    }
+}
+
+function createEditFormHTML(artist: Artist): string {
+    return /*html*/ `
+    <h2>Edit Artist</h2>
+    <form id="edit-artist-form">
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" value="${artist.name}" required>
+      <label for="birthday">Birthday</label>
+      <input type="date" id="birthday" name="birthday" value="${artist.birthday}" required>
+      <label for="activeSince">Active Since</label>
+      <input type="number" id="activeSince" name="activeSince" value="${artist.activeSince}" required>
+      <label for="genres">Genres</label>
+      <input type="text" id="genres" name="genres" value="${artist.genres}" required>
+      <label for="labels">Labels</label>
+      <input type="text" id="labels" name="labels" value="${artist.labels}" required>
+      <label for="website">Website</label>
+      <input type="text" id="website" name="website" value="${artist.website}" required>
+      <label for="shortDescription">Short Description</label>
+      <input type="text" id="shortDescription" name="shortDescription" value="${artist.shortDescription}" required>
+      <label for="image">Image</label>
+      <input type="text" id="image" name="image" value="${artist.image}" required>
+      <button type="submit">Update</button>
+    </form>
+  `;
+}
+
+async function updateArtist(e: Event, modal: HTMLDialogElement, id: number): Promise<void> {
     e.preventDefault();
-    const form = e.target as HTMLFormElement;
+
     const updatedArtist: Artist = {
         name: getValue('#name'),
         birthday: getValue('#birthday'),
@@ -55,19 +76,25 @@ async function updateArtist(e: Event, modal: HTMLDialogElement, id: number) {
         shortDescription: getValue('#shortDescription'),
         image: getValue('#image'),
     };
+
     const response = await update('artists', id.toString(), updatedArtist);
+
     if (response instanceof Error) {
         console.error('Error updating artist');
         return;
     }
+
     modal.close();
-    // TODO: Visual feedback that the artist was created
+
+    // TODO: Visual feedback that the artist was updated
+
     // Refresh the artist list
     await refreshArtists(response);
+}
 
 function getValue(selector: string) {
-    const element = form.querySelector(selector) as HTMLInputElement;
+    const element = document.querySelector(selector) as HTMLInputElement;
     return element.value;
 }
-}
-export {editArtist};
+
+export { editArtist };
